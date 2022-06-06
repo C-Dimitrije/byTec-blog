466ded10652a0bc149d38fc83720cb52fef04d39
DimitrijeCvet <dimitrije.le.cvetkovic@gmail.com>
2022-06-06 05:24:07 +0200

    asd

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories, Loader, Header } from '../../components';
+
+const CategoryPost = ({ posts }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+    <Head>
+    <link rel="preconnect" href="https://fonts.gstatic.com"  />
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&family=Ubuntu:wght@300&display=swap"
+        rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous"></link>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossOrigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/dd4ecbb228.js" crossOrigin="anonymous"></script>
+    </Head>
+    <div className="display">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+export default CategoryPost;
+
+// Fetch data at build time
+export async function getStaticProps({ params }) {
+  const posts = await getCategoryPost(params.slug);
+
+  return {
+    props: { posts },
+  };
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
+  const categories = await getCategories();
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
